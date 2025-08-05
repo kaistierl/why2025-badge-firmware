c0abda8a724a3d766734183dc7df703c7c493fe4
Francis Duvivier <francisduvivier@gmail.com>
2025-08-05 21:22:52 +0200

    Add log back to main thread

--- sdk_apps/hello/hello.c
+++ sdk_apps/hello/hello.c
@@ -1,5 +1,4 @@
 #include "badgevms/process.h"
-#include <time.h>
 
 #include <stdatomic.h> // for atomic variables
 #include <stdbool.h>   // for bool type
@@ -10,18 +9,39 @@
 #include <unistd.h> // for usleep
 
 // The current version of this application.
-int const CURRENT_VERSION = 20;
+int const CURRENT_VERSION = 21;
 
-atomic_bool should_stop     = false;
+bool should_stop        = false;
+int         process_start_time = -1;
+
+/**
+ * @brief The main entry point of the application.
+ *
+ * It starts the version checker thread and then enters a loop that prints a
+ * message every second until the 'should_stop' flag is set to true by the
+ * other thread.
+ */
+time_t unix_timestamp;
+
+void version_logger_thread(void *data) {
+    while (true) {
+        printf("BADGEHUB VLOG: APP V[%d] started at[%ld], ", process_start_time, (long)unix_timestamp);
+        usleep(5 * 1000 * 1000);
+    }
+}
 
 int main(int argc, char *argv[]) {
+    unix_timestamp = time(NULL);
+    printf("The current Unix timestamp is: %ld\n", (long)unix_timestamp);
+    thread_create(version_logger_thread, NULL, 4096);
 
     // Loop until the version_checker_thread signals us to stop.
-    while (!atomic_load(&should_stop)) {
-        printf("Hello BadgeVMS world! V%d!\n", CURRENT_VERSION);
+    while (!should_stop) {
+        // TODO badgehub loop?
         int one_second_in_usec = 1000 * 1000;
         // Sleep for one second
         usleep(one_second_in_usec);
+        printf("BADGEHUB MAIN: APP V[%d] started at[%ld], ", process_start_time, (long)unix_timestamp);
     }
 
     printf("Exiting due to version update.\n");
