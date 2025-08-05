2c0c972dfc2691d4c357923f0cd6b8bca12192b9
Francis Duvivier <francisduvivier@gmail.com>
2025-08-05 22:20:57 +0200

    invalidate token and move it to env

--- upload.sh
+++ upload.sh
@@ -1,11 +1,16 @@
 idf.py build;
-curl -X POST -H "badgehub-api-token: 195daa5cdf961b4a24dea85ec6f773da" \
--F "file=@./build/app_elfs/hello.elf" \
-https://badge.why2025.org/api/v3/projects/badgehub_dev/draft/files/hello.elf
-echo "did upload"
+PROJECT_NAME=badgehub_dev
+LOCAL_ELF_NAME=badgehub.elf
+REMOTE_ELF_NAME=hello.elf
+#BADGEHUB_PROJECT_API_TOKEN=TODO_EXPORT_IN_ENV_MANUALLY
 
-curl -X 'PATCH' -H "badgehub-api-token: 195daa5cdf961b4a24dea85ec6f773da" \
-  'https://badge.why2025.org/api/v3/projects/badgehub_dev/publish' \
+curl -X POST -H "badgehub-api-token: ${BADGEHUB_PROJECT_API_TOKEN}" \
+-F "file=@./build/app_elfs/${LOCAL_ELF_NAME}" \
+https://badge.why2025.org/api/v3/projects/${PROJECT_NAME}/draft/files/${REMOTE_ELF_NAME}
+echo "did upload badgehub.elf to project ${PROJECT_NAME}"
+
+curl -X 'PATCH' -H "badgehub-api-token: ${BADGEHUB_PROJECT_API_TOKEN}" \
+  "https://badge.why2025.org/api/v3/projects/${$PROJECT_NAME}/publish" \
   -H 'accept: application/json' \
   -H 'Content-Type: application/json'
 
