11c6887c64b4d9f6470c9c8ebd55665629edb527
Kai Stierl <kai@kaistierl.de>
2025-08-31 17:21:11 +0200

    Add support for ESP32 hardware RNG

---
+++ badgevms/crypto.c
@@ -0,0 +1,26 @@
+/* This file is part of BadgeVMS
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "badgevms/crypto.h"
+#include "esp_random.h"
+
+uint32_t crypto_esp_random(void) {
+    return esp_random();
+}
+
+void crypto_esp_fill_random(void *buf, uint32_t len) {
+    esp_fill_random(buf, (size_t)len);
+}
